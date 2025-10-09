be458e0240623e9c5d85a56c6409559eeb2827d6
rayanweragala <ryanthilakshana2000@gmail.com>
2025-10-09 21:58:25 +0530

    Merged GitHub remote into local Monorepo structure

---
+++ backend/src/test/java/com/localshare/backend/LocalShareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localshare.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalShareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
