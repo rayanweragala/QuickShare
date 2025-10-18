45ca4fd348ca059bf0dc0487a61a3c9ba45aeabc
rayanweragala <110955943+rayanweragala@users.noreply.github.com>
2025-10-18 09:02:01 +0530

    Merged GitHub remote into local Monorepo structure

---
+++ backend/src/main/java/com/localshare/backend/LocalShareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.localshare.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalShareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalShareBackendApplication.class, args);
+	}
+
+}
